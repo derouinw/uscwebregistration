ea9a36a67b5ef381060eff33bdb5d0535c6c6b87
Bill Derouin <derouinbill@gmail.com>
2015-02-07 20:08:27 -0800

    Added volley, beginning of courses display

---
+++ app/src/main/java/com/grilla/uscwebregistration/CoursesFragment.java
@@ -0,0 +1,60 @@
+package com.grilla.uscwebregistration;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CoursesFragment extends Fragment {
+    public String ARG_SCHOOL = "com.grilla.uscwebregistration.ARG_SCHOOL";
+    private String school;
+
+    public CoursesFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_courses, container, false);
+
+        school = savedInstanceState.getString(ARG_SCHOOL);
+        String request = "http://petri.usc.edu/socAPI/Courses/" + JSONHelper.term + "/" + school;
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, request,
+                new Response.Listener<String>() {
+
+                    public void onResponse(String response) {
+                        System.out.println("Response is: " + response);
+
+                        try {
+                            JSONObject jo = new JSONObject(response);
+
+                            // Load cards with courses
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/grilla/uscwebregistration/JSONHelper.java
@@ -0,0 +1,8 @@
+package com.grilla.uscwebregistration;
+
+/**
+ * Created by bill on 2/7/15.
+ */
+public class JSONHelper {
+    public static String term = "20151";
+}
