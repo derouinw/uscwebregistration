3febc3d37ee2d556bbcd2590c04d481d5792fdc0
Bill Derouin <derouinbill@gmail.com>
2015-02-21 17:02:41 -0800

    Load data progress

--- app/src/main/java/com/grilla/uscwebregistration/DepartmentsFragment.java
+++ app/src/main/java/com/grilla/uscwebregistration/DepartmentsFragment.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +13,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DepartmentsFragment extends Fragment {
-    public final static String ARG_DEPARTMENTS = "com.grilla.uscwebregistration.ARG_DEPARTMENTS";
     public static final String ARG_SCHOOL_CODE = "com.grilla.uscwebregistration.ARG_SCHOOL_CODE";
 
     private String[] departments;
+    private String school;
 
     public DepartmentsFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (savedInstanceState == null) savedInstanceState = getArguments();
-        if (savedInstanceState != null) departments = savedInstanceState.getStringArray(ARG_DEPARTMENTS);
+        if (savedInstanceState != null) school = savedInstanceState.getString(ARG_SCHOOL_CODE);
 
         Context c = getActivity().getApplicationContext();
         View rootView = inflater.inflate(R.layout.fragment_departments, container, false);
-
-        if (savedInstanceState == null) savedInstanceState = getArguments();
-        if (savedInstanceState != null) departments = savedInstanceState.getStringArray(ARG_DEPARTMENTS);
 
         ListView schoolsList = (ListView)rootView.findViewById(R.id.departments_list);
         ArrayAdapter<String> arrayAdpt = new ArrayAdapter<>(c, R.layout.schools_list_item, departments);
@@ -51,6 +58,35 @@ public class DepartmentsFragment extends Fragment {
             }
         });
 
+        RequestQueue queue = JSONSingleton.getInstance(c).getRequestQueue();
+        String request = JSONHelper.SCHOOLS_URL + school;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, request,
+                new Response.Listener<String>() {
+
+                    public void onResponse(String response) {
+                        Log.d("", ""); // optional log message
+
+                        try {
+                            JSONObject jo = new JSONObject(response);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {}
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
         return rootView;
+    }
+
+    private void loadDepartmentsData(JSONObject jo) {
+
     }
 }
